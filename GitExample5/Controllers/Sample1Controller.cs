d28cea017f1ee409741690e9472b5da5d4984c89
Rashmi R S <rashmirangana.surpu@hcl.com>
2025-11-13 15:39:55 +0530

    Add project files.

---
+++ GitExample5/Controllers/Sample1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitExample5.Controllers
+{
+    public class Sample1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
