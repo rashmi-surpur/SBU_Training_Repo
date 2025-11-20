ceb30d028aa34f1e3a5ad7d6f04278190344c12a
Rashmi R S <rashmirangana.surpu@hcl.com>
2025-11-20 15:47:33 +0530

    Branch2- updated

--- GitExample5/Controllers/HomeController.cs
+++ GitExample5/Controllers/HomeController.cs
@@ -29,6 +29,10 @@ namespace GitExample5.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public IActionResult Page3()
+        {
+            return View();
+        }
 
     }
 }
